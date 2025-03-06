c59917eb47e5eb1c2c3ccd4e1d39b55ff1bf7cc5
Vlad <10ner11.sc@gmail.com>
2025-03-06 03:28:51 +0300

    public void saveUser     public void removeUserById

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -18,14 +18,19 @@ public class UserDaoJDBCImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-        String sql = "CREATE TABLE IF NOT EXISTS public.users\n" +
-                "(\n" +
-                "    id integer NOT NULL DEFAULT nextval('users_id_seq'::regclass),\n" +
-                "    name character varying(50) COLLATE pg_catalog.\"default\" NOT NULL,\n" +
-                "    lastname character varying(50) COLLATE pg_catalog.\"default\" NOT NULL,\n" +
-                "    age smallint NOT NULL,\n" +
-                "    CONSTRAINT users_pkey PRIMARY KEY (id, name, lastname, age)\n" +
-                ")\n";
+//        String sql = "CREATE TABLE IF NOT EXISTS public.users\n" +
+//                "(\n" +
+//                "    id integer NOT NULL DEFAULT nextval('users_id_seq'::regclass),\n" +
+//                "    name character varying(50) COLLATE pg_catalog.\"default\" NOT NULL,\n" +
+//                "    lastname character varying(50) COLLATE pg_catalog.\"default\" NOT NULL,\n" +
+//                "    age smallint NOT NULL,\n" +
+//                "    CONSTRAINT users_pkey PRIMARY KEY (id, name, lastname, age)\n" +
+//                ")\n";
+        String sql = "CREATE TABLE users (" +
+                "id INTEGER NOT NULL PRIMARY KEY" +
+                "name VARCHAR(64) NOT NULL" +
+                "last_name VARCHAR(64) NOT NULL" +
+                "age SMALLINT not NULL";
 
 
         try (Statement stmt = conn.createStatement()) {
@@ -50,16 +55,27 @@ public class UserDaoJDBCImpl implements UserDao {
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        String sql = "INSERT INTO users (name, last_name, age) VALUES (" + name + "," + lastName + "," + age + ")";
+        try (Statement stmt = conn.createStatement()) {
+            stmt.execute(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-
+        String sql = "DELETE FROM users WHERE id = " + id;
+        try (Statement stmt = conn.createStatement()) {
+            stmt.execute(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
+
         return null;
     }
 

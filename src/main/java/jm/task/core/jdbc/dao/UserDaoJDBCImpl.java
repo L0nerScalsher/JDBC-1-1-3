232ad77547ca4b2414389f1aaea28ba3dba16c0f
Vlad <10ner11.sc@gmail.com>
2025-03-06 03:05:58 +0300

    Utils createUsersTable() dropUsersTable()

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,34 +1,69 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
+
+    private final Connection conn = Util.getConnection();
+
     public UserDaoJDBCImpl() {
 
     }
 
+    @Override
     public void createUsersTable() {
+        String sql = "CREATE TABLE IF NOT EXISTS public.users\n" +
+                "(\n" +
+                "    id integer NOT NULL DEFAULT nextval('users_id_seq'::regclass),\n" +
+                "    name character varying(50) COLLATE pg_catalog.\"default\" NOT NULL,\n" +
+                "    lastname character varying(50) COLLATE pg_catalog.\"default\" NOT NULL,\n" +
+                "    age smallint NOT NULL,\n" +
+                "    CONSTRAINT users_pkey PRIMARY KEY (id, name, lastname, age)\n" +
+                ")\n";
+
+
+        try (Statement stmt = conn.createStatement()) {
+            stmt.execute(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 
+    @Override
     public void dropUsersTable() {
+        String sql = "DROP TABLE IF EXISTS users;";
+
+        try (Statement stmt = conn.createStatement()) {
+            stmt.execute(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 
+    @Override
     public void saveUser(String name, String lastName, byte age) {
 
     }
 
+    @Override
     public void removeUserById(long id) {
 
     }
 
+    @Override
     public List<User> getAllUsers() {
         return null;
     }
 
+    @Override
     public void cleanUsersTable() {
 
     }
